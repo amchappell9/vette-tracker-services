af1b83f037a204d3d01b7af84f05143c159167dc
Austin Chappell <amchappell9@gmail.com>
2025-04-01 20:57:56 -0400

    Testing deploy process

--- internal/handlers/utilHandlers.go
+++ internal/handlers/utilHandlers.go
@@ -3,5 +3,5 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 func PingHandler(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "pong"})
+	c.JSON(200, gin.H{"message": "pong goes the bell"})
 }
